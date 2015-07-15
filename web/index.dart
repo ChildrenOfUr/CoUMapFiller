4dbe333a4a74e095df17d5e951dcef418a9b7559
klikini <klikini@robiotic.net>
2015-07-15 18:55:39 -0500

    add shoppe door WARNING: NOW CONNECTS TO LOCALHOST

--- web/index.dart
+++ web/index.dart
@@ -24,7 +24,7 @@ part 'shrines_and_vendors.dart';
 part 'maps_data.dart';
 
 String currentLayer = "EntityHolder", tsid, initialPopupWidth, initialPopupHeight;
-String serverAddress = "http://robertmcdermot.com:8181";
+String serverAddress = "http://localhost:8181";
 int width = 3000 , height = 1000;
 DivElement gameScreen, layers;
 Rectangle bounds;
